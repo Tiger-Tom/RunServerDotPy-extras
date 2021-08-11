d57b8c5d6404ea8dff1c78682cafb327a84985d5
TigerTom <55163690+Tiger-Tom@users.noreply.github.com>
2021-08-11 16:44:46 -0700

    Create perm1-Example.py

---
+++ ModExamples/perm1-Example.py
@@ -0,0 +1,28 @@
+#Mods can add new ChatCommands and new help texts.
+#They can also modify current ChatCommands and help text.
+#ChatCommands are loaded A-Z.
+#Output syntax:
+#mod_output_ = (help texts (dictionary), chat command funcs (dictionary), perm level (integer, 0-2 (0=any user, 1=admin only, 2=root user)))
+
+# Permission level for all defined ChatCommands in this mod
+permLevel = 1 #0=any user ; 1=admin user ; 2=root/console only
+
+# Example ChatCommand
+def chatCommand(args, user):
+    tellRaw('Admin Test:\n'+str(args), 'FromText', user)
+def moddedChatCommand(args, user):
+    tellRaw('Admin Modified help (user='+str(user)+', args='+str(args)+')', None, user)
+    writeCommand('say Test')
+
+# Example help text
+helpTxts = {
+    'chatcommandtest | cctest': 'A test command, loaded through a mod!',
+    'help [*command]': 'The modified "help" command, through a mod',
+}
+chatCommandFuncs = {
+    'chatcommandtest': chatCommand,
+    'cctest': chatCommand, #Multiple can point to the same command
+}
+
+# Return the data
+mod_output_ = (helpTxts, chatCommandFuncs, permLevel) #You MUST use "mod_output_ = [your outputs]" here, as anything else won't work!
