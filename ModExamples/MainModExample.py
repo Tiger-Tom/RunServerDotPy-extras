d2d84ce2bed7c384e20103cdfea954a66ea554ee
TigerTom <55163690+Tiger-Tom@users.noreply.github.com>
2021-10-02 20:21:13 -0700

    Modding overhaul

--- ModExamples/MainModExample.py
+++ ModExamples/MainModExample.py
@@ -2,30 +2,27 @@
 #They can also modify current ChatCommands and help text.
 #They can also run any functions that the normal code can, so be careful when loading them
 #ChatCommands are loaded A-Z.
+#All of the code in "load_mod" will be used to load the mod. It will be passed 2 arguments:
+# An array of dictionaries as the help texts (the 0th index will be base level permission commands, 1st index will be sudo, 2nd will be root)
+# An array of dictionaries as the ChatCommands (indexes same as with help texts)
+#Nothing will be done with any returned values
 #Output syntax:
-# global mod_output_
-# mod_output_ = (help texts (dictionary), chat command funcs (dictionary), perm level (integer, 0-2 (0=any user, 1=admin only, 2=root user)))
+#def load_mod(helpTxts, chatCommandFuncs):
+#   [your code]
 
-# Permission level for all defined ChatCommands in this mod
-permLevel = 0 #0=any user ; 1=admin user ; 2=root/console only
+def load_mod(helpTxts, chatCommandFuncs):
+    # Setup ChatCommands
+    def chatCommand(args, user):
+        tellRaw('Test:\n'+str(args), 'FromText', user)
+    def moddedChatCommand(args, user):
+        tellRaw('Modified help (user='+str(user)+', args='+str(args)+')', None, user)
+        writeCommand('say Test')
 
-# Example ChatCommand
-def chatCommand(args, user):
-    tellRaw('Test:\n'+str(args), 'FromText', user)
-def moddedChatCommand(args, user):
-    tellRaw('Modified help (user='+str(user)+', args='+str(args)+')', None, user)
-    writeCommand('say Test')
-
-# Example help text
-helpTxts = {
-    'chatcommandtest | cctest': 'A test command, loaded through a mod',
-    'help [command*]': 'The modified help command, changed through a mod',
-}
-chatCommandFuncs = {
-    'chatcommandtest': chatCommand,
-    'cctest': chatCommand, #Multiple can point to the same command
-}
-
-# Return the data
-global mod_output_ #"mod_output_" MUST be global
-mod_output_ = (helpTxts, chatCommandFuncs, permLevel) #You MUST use "mod_output_ = [your outputs]" here, as anything else won't work!
+    # Apply ChatCommands
+    chatCommandFuncs[0]['chatcommandtest'] = chatCommand
+    chatCommandFuncs[0]['cctest'] = chatCommand
+    chatCommandFuncs[0]['help'] = moddedChatCommand
+    
+    # Apply help texts
+    helpTxts[0]['chatcommandtest | cctest'] = 'A test command, loaded through a mod'
+    helpTxts[0]['help [command*]'] = 'The modified help command'
