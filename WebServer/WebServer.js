ec6b2dc832d2acbce9a4b5f10a41d3596317ac8a
TigerTom <55163690+Tiger-Tom@users.noreply.github.com>
2021-10-05 19:18:18 -0700

    Update WebServer.js

--- WebServer/WebServer.js
+++ WebServer/WebServer.js
@@ -1,5 +1,3 @@
 window.onload = function() {
-    sock = new WebSocket('ws://localhost:{8079}');
-    sock.onmessage = function() { document.body.innerHTML += '<br>'+event.data.replaceAll('\n', '<br>') };
-    sock.onopen = function() { sock.send('Hello, server!') };
+    fetch('localhost:{%PORT}').then(response => response.text()).then(data => console.log(data))
 }
